709b0e6f278f74f2e37814449a4eca4c4318d367
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-10-11 23:08:39 +0200

    Tab bar navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,36 @@ import { Ionicons }  from "@expo/vector-icons"
 export default function TabsLayout() {
   return (
       <Tabs screenOptions={{
-        tabBarActiveTintColor: "#ffd33d"
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+            backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+            backgroundColor: '#25292e'
+        }
       }}>
-        <Tabs.Screen name="index" options={{ headerShown:true, headerTitle: "Money Goal", tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "home-sharp" : "home-outline"} color={color} size={30}/>}}/>
-        <Tabs.Screen name="about" options={{ headerShown:true, title: "About",tabBarIcon: ({focused, color}) => <Ionicons name={focused ? "home-sharp" : "home-outline"} color={color} size={30}/>}}/>
-      </Tabs>
+        <Tabs.Screen 
+            name="index" 
+            options={{ 
+                headerShown:true, 
+                headerTitle: "Money Goal", 
+                tabBarIcon: ({focused, color}) => <Ionicons 
+                    name={focused ? "home-sharp" : "home-outline"} 
+                    color={color} 
+                    size={30}/>}}
+        />
+        <Tabs.Screen 
+            name="about" 
+            options={{ 
+                headerShown:true, 
+                title: "About",
+                tabBarIcon: ({focused, color}) => <Ionicons 
+                    name={focused ? 'information-circle' : 'information-circle-outline'} 
+                    color={color} 
+                    size={30}/>}}
+        />
+      </Tabs>     
   );
 }
